cf12ca3694137766b3aeba70814d0cac9aabe35d
Arthur Ribeiro <arthurmr13@hotmail.com>
2023-01-10 17:46:41 -0300

    Fixed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
 export const OPENSEA_FEE = 0.025
 
 export const calculateProfit = (amountToBuy: number, profit: number) => {
-  console.log('amountToBuy', amountToBuy)
-    console.log('profit', profit)
   return amountToBuy * (profit / 100)
 }
