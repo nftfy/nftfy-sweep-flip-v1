c1434a53546d3caf6754c15de397e1abd1ff64fb
Arthur Ribeiro <arthurmr13@hotmail.com>
2023-01-10 12:46:41 -0300

    Fixed layout

--- src/components/CheckoutModal.tsx
+++ src/components/CheckoutModal.tsx
@@ -135,7 +135,7 @@ export function CheckoutModal({
             </TokenContainer>
           </Col>
           <Col style={{ width: '100%' }}>
-            <Card
+            <Card size='small'
               title={
                 <>
                   <Row style={{ justifyContent: 'space-between' }}>
@@ -187,19 +187,18 @@ export function CheckoutModal({
               </>
             </Card>
 
-            <Card style={{ width: '100%' }}>
+            <Card size='small' style={{ width: '100%' }}>
               <div
                 style={{
-                  marginLeft: '-7px',
                   marginBottom: '8px',
                   backgroundColor: 'var(--green-1)',
                   border: '1px solid var(--green-6)',
                   borderRadius: '12px',
-                  padding: '5px 8px'
+                  padding: '5px 12px'
                 }}
               >
                 <CardContainer>
-                  <Col style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                  <Col style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                     <Text type='secondary'>Target Profit</Text>
                     <Text type='secondary'>Expected Profit</Text>
                   </Col>
@@ -210,13 +209,13 @@ export function CheckoutModal({
                 </CardContainer>
               </div>
               <CardContainer>
-                <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', padding: '0 12px' }}>
                   <Text type='secondary'>You will receive</Text>
                   <Text type='secondary'>Each NFT will be relisted at</Text>
                   <Text type='secondary'>Collection royalty</Text>
                   <Text type='secondary'>{nameCollectionSize > 22 ? `${collection?.name?.slice(0, 22)}...` : collection?.name} fee</Text>
                 </div>
-                <div style={{ display: 'flex', alignItems: 'flex-end', flexDirection: 'column', gap: '8px' }}>
+                <div style={{ display: 'flex', alignItems: 'flex-end', flexDirection: 'column', gap: '10px', padding: '0 12px' }}>
                   <Text>{expectedProfit?.toFixed(3)} ETH</Text>
                   <Text>{salePrice.toFixed(3)} ETH</Text>
                   <Text>{buyRoyality} ETH</Text>
